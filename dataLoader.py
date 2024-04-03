78bd9b215641376ce9df0fe4e63531eacea1b0a5
sylvainshan <sylvain.teff@gmail.com>
2024-04-03 12:18:16 +0200

    Update dataLoader.py

--- dataLoader.py
+++ dataLoader.py
@@ -1,11 +1,20 @@
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
-from torch.utils.data import Dataset
-import torch
 import pickle
 
+import torch
+import torchvision
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
 
 # dataset site : https://www.cs.toronto.edu/~kriz/cifar.html
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize(mean=[0.49139968, 0.48215841, 0.44653091], std=[0.24703223, 0.24348513, 0.26158784])])
+
+train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
 
 class CIFAR10Dataset(Dataset):
@@ -20,105 +29,50 @@ class CIFAR10Dataset(Dataset):
         return self.images[idx], self.labels[idx]
 
 
-
-#
-# def unpickle(file):
-#     with open(file, 'rb') as fo:
-#         dict = pickle.load(fo, encoding='bytes')
-#     return dict
-#
-#
-#
-# def load_batches():
-#     batch_1 = unpickle('./cifar-10-batches-py/data_batch_1')
-#     batch_2 = unpickle('./cifar-10-batches-py/data_batch_2')
-#     batch_3 = unpickle('./cifar-10-batches-py/data_batch_3')
-#     batch_4 = unpickle('./cifar-10-batches-py/data_batch_4')
-#     batch_5 = unpickle('./cifar-10-batches-py/data_batch_5')
-#
-#     test_batch = unpickle('./cifar-10-batches-py/test_batch')
-#
-#     train_batches = [batch_1, batch_2, batch_3, batch_4, batch_5]
-#     return train_batches, test_batch
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
 
-def load_images_labels(n_batches = 5):
+DIR = './data/cifar-10-batches-py/'
+
+
+def load_batches():
+    print(f'{DIR}data_batch_{1}')
+    train_batches = [unpickle(f'{DIR}data_batch_{i}') for i in range(1, 6)]
+    test_batch = unpickle(f'{DIR}test_batch')
+
+    return train_batches, test_batch
+
+
+def load_images_labels(n_batches=5):
     train_batches, test_batch = load_batches()
 
-    train_images = normalize_image(np.concatenate([np.array(train_batches[i][b'data']) for i in range(n_batches)], axis=0)).astype(np.float32)
-    train_labels = np.concatenate([np.array(train_batches[i][b'labels']) for i in range(n_batches)], axis=0).astype(np.float32)
+    train_images = normalize_image(
+        np.concatenate([np.array(train_batches[i][b'data']) for i in range(n_batches)], axis=0)).astype(np.float32)
+    train_labels = np.concatenate([np.array(train_batches[i][b'labels']) for i in range(n_batches)], axis=0).astype(
+        np.float32)
 
     test_images = normalize_image(np.array(test_batch[b'data'])).astype(np.float32)
     test_labels = np.array(test_batch[b'labels']).astype(np.float32)
-
-
 
     # print(train_images[0]) test
 
     # make labels to one_hot encoding
     encoder = OneHotEncoder()
-    one_hot_train_labels = encoder.fit_transform(train_labels.reshape(-1,1)).toarray()
-    one_hot_test_labels = encoder.transform(test_labels.reshape(-1,1)).toarray()
+    one_hot_train_labels = encoder.fit_transform(train_labels.reshape(-1, 1)).toarray()
+    one_hot_test_labels = encoder.transform(test_labels.reshape(-1, 1)).toarray()
 
     train_dataset = CIFAR10Dataset(train_images, one_hot_train_labels)
     test_dataset = CIFAR10Dataset(test_images, one_hot_test_labels)
-
 
     return train_dataset, test_dataset
 
 
 def normalize_image(image):
-    return image/255.0
+    return image / 255.0
+
 
 if __name__ == '__main__':
-
-    a,b = load_images_labels(1)
-
-    print(a[0][0][0])
-
-
-
-
-    # images = batch_1[b'data']
-    # labels = batch_1[b'labels']
-
-    # print(images.shape, len(labels))
-
-
-import torch
-import torchvision
-import torchvision.transforms as transforms
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize(mean=[0.49139968, 0.48215841, 0.44653091], std=[0.24703223, 0.24348513, 0.26158784])])
-
-train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-
-
-def load_images_labels():
-    train_images = train_set.data
-    test_images = test_set.data
-
-    train_labels = train_set.targets
-    test_labels = test_set.targets
-
-    encoder = OneHotEncoder()
-    one_hot_train_labels = encoder.fit_transform(train_labels.reshape(-1,1)).toarray()
-    one_hot_test_labels = encoder.transform(test_labels.reshape(-1,1)).toarray()
-
-    train_dataset = CIFAR10Dataset(train_images, one_hot_train_labels)
-    test_dataset = CIFAR10Dataset(test_images, one_hot_test_labels)
-
-    return train_dataset, test_dataset
-
-# print("Shape of the training set:", train_set.data.shape)
-# print("Shape of the test set:", test_set.data.shape)
-
-# # Image format : (color channels, height, width)
-# train_images = torch.stack([train_set[i][0] for i in range(len(train_set))])
-# train_labels = torch.tensor([train_set[i][1] for i in range(len(train_set))])
-#
-# test_images = torch.stack([test_set[i][0] for i in range(len(test_set))])
-# test_labels = torch.tensor([test_set[i][1] for i in range(len(test_set))])
+    print(" ")
