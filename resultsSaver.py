55f07360b6dae9104c0cbbbe4cadd3c7f55064db
Robin <robin.96@hotmail.com>
2024-05-27 18:09:22 +0200

    resultsSaver class

---
+++ resultsSaver.py
@@ -0,0 +1,50 @@
+import json
+
+class sharpnessResultsSaver:
+    def __init__(self, effect = None, effect_name = None, list_of_sharpness = None, list_of_values = None, errors = None, model_class_name='MLP_1', optimizer='SGD', n_iterations=5, save = True):
+        
+        if save:
+            self.effect = effect
+            self.effect_name = effect_name
+            self.list_of_sharpness = list_of_sharpness
+            self.list_of_values = list_of_values
+            self.errors = errors
+            self.model_class_name = model_class_name
+            self.optimizer = optimizer
+            self.n_iterations = n_iterations
+            
+            assert effect is not None
+            assert effect_name is not None
+            assert list_of_sharpness is not None
+            assert list_of_values is not None
+            assert errors is not None
+            assert len(self.list_of_values) == len(self.list_of_sharpness)
+            assert len(self.list_of_values) == len(self.errors)
+
+            self.saveResultToJSON()
+
+    def saveResultToJSON(self):
+        result = {
+            "effect": self.effect,
+            "effect_name": self.effect_name,
+            "list_of_sharpness": self.list_of_sharpness,
+            "list_of_values": self.list_of_values,
+            "errors": self.errors,
+            "model_class_name": self.model_class_name,
+            "optimizer": self.optimizer,
+            "n_iterations": self.n_iterations
+        }
+        # save to json file
+        fileName = self.model_class_name + "_" + self.effect_name + ".json"
+        with open(f'{fileName}.json', 'w') as f:
+            json.dump(result, f, indent=4)
+
+    def getResultFromFile(self):
+        fileName = self.model_class_name + "_" + self.effect_name + ".json"
+        with open(f'{fileName}.json', 'r') as f:
+            result = json.load(f)
+        
+        return result
+    
+    def printResult(self):
+        fileName = self.model_class_name + "_" + self.effect_name + ".json"
